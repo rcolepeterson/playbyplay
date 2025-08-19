7b55a66480ace6070db4bdd43e8aecc73508f2c0
Cole Peterson <rcolepeterson@gmail.com>
2025-08-19 13:52:08 -0700

    refactor: simplify Page component and retain TTS demo documentation

--- app/comentary/page.tsx
+++ app/comentary/page.tsx
@@ -1,10 +1,8 @@
+/*
+This file holds a demo component for testing Azure Text-to-Speech (TTS) functionality in the browser. It allows you to synthesize speech using Azure’s TTS API, but is not used in the main app flow, which relies on Eleven Labs for production TTS features.
+*/
+
 import Comentary from "./comentary-ui";
-const Page = () => {
-  return (
-    <div>
-      <Comentary />
-    </div>
-  );
-};
+const Page = () => <Comentary />;
 
 export default Page;
